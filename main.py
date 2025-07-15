91dde9c18346c90ab57650845a3291281051e552
The_Legolas <lucalegom@gmail.com>
2025-07-15 13:27:50 +0200

    created the astroids and the asteroid_field

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 import pygame
 from constants import *
-from player import Player  # Assuming player.py is in the same directory
-
-
+from player import Player 
+from asteroid import Asteroid 
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    pygame.display.set_caption("Asteroids")
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # Initialize player at the center of the screen
+
     dt = 0
 
-    pygame.display.set_caption("Asteroids")
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)  # Initialize player at the center of the screen
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        player.update(dt)
+        updatable.update(dt)
 
         screen.fill((0, 0, 0))  # Fill the screen with black
-        player.draw(screen) # Draw the player
+        
+        for obj in drawable:
+            obj.draw(screen) # Draw the player
+
         pygame.display.flip()  # Update the display
 
         # limit the framerate to 60 FPS
